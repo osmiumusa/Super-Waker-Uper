283451383f7170562ef5172604d8496cd1e64668
Tucker <osmiumusa@gmail.com>
2015-11-15 20:28:50 -0500

    Initial Commit

---
+++ app/src/main/java/com/osmiumusa/superwakeruper/TimePickerFragment.java
@@ -0,0 +1,34 @@
+package com.osmiumusa.superwakeruper;
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.text.format.DateFormat;
+import android.widget.TextView;
+import android.app.DialogFragment;
+import android.app.Dialog;
+import java.util.Calendar;
+import android.widget.TimePicker;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener{
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        //Use the current time as the default values for the time picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        //Create and return a new instance of TimePickerDialog
+        return new TimePickerDialog(getActivity(),this, hour, minute,true);
+    }
+
+    //onTimeSet() callback method
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute){
+        ((MainActivity)getActivity()).clockcallback(hourOfDay,minute);
+    }
+}
